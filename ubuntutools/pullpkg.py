lubuntu-team/ubuntu-dev-tools

commit 2f396fe54956c85e0f0e62891f29dc7bab7d110b

When using pull-*-source to grab a package which already has a defined Vcs- field, display the exact same warning message `apt source` does.

ubuntutools/pullpkg.py
@@ -445,6 +445,37 @@ class PullPkg:
 
         Logger.info("Found %s", spph.display_name)
 
+        # The VCS detection logic was modeled after `apt source`
+        for key in srcpkg.dsc.keys():
+            original_key = key
+            key = key.lower()
+
+            if key.startswith("vcs-"):
+                if key == "vcs-browser":
+                    continue
+                elif key == "vcs-git":
+                    vcs = "Git"
+                elif key == "vcs-bzr":
+                    vcs = "Bazaar"
+                else:
+                    continue
+
+                uri = srcpkg.dsc[original_key]
+
+                Logger.warning("\nNOTICE: '%s' packaging is maintained in "
+                               "the '%s' version control system at:\n"
+                               " %s\n" % (package, vcs, uri))
+
+                if vcs == "Bazaar":
+                    vcscmd = " $ bzr branch " + uri
+                elif vcs == "Git":
+                    vcscmd = " $ git clone " + uri
+
+                if vcscmd:
+                    Logger.info(f"Please use:\n{vcscmd}\n"
+                                "to retrieve the latest (possibly unreleased) "
+                                "updates to the package.\n")
+
         if pull == PULL_LIST:
             Logger.info("Source files:")
             for f in srcpkg.dsc["Files"]:

commit 41e7d2d714217b7be7cabfc45176df38a3cd5c2d

ubuntutools/pullpkg.py: initialize vcscmd
Fixes: 2f396fe54956

ubuntutools/pullpkg.py
@@ -471,6 +471,7 @@ class PullPkg:
                     uri,
                 )
 
+                vcscmd = ""
                 if vcs == "Bazaar":
                     vcscmd = " $ bzr branch " + uri
                 elif vcs == "Git":